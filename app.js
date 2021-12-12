0228d537202472e328580ecf94b294dcc20b052b
jacksonarnold27 <jacksonarnold27@gmail.com>
2021-12-12 15:01:04 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const PI = 3.14;
+PI = 42;
+
+// What is the difference between var and let?
+// var is declared in function scope and let is declared in block scope. var is also redeclarable whereas let is not
+
+
+
+// What is the difference between var and const?
+// var is declared in function scope and can be redeclared and reassigned. const is declared in block scope and cannot do either of those things.
+
+// What is the difference between let and const?
+// let can be reassigned, const can not
+
+// What is hoisting?
+// the concept of the JS interpreter moving all variable and function declarations to the 'top of the script'
+// essentially variables and functions are declared first when the code is executed
